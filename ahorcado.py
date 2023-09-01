24f53e11323c52ea7133a0181b544d5d797a0bb0
sergioFavio <serginho61@gmail.com>
2023-08-31 20:15:39 -0400

    version 20223-08-31_01

--- ahorcado.py
+++ ahorcado.py
@@ -3,6 +3,15 @@ def jugar():
   print('Bienvenido al Juego del Ahorcado')
   print('================================')
 
+  palabra_secreta ='mandarina'
+  
+  ahorcado = False
+  acerto = False
+  
+  while(not ahorcado and not acerto):
+    print('jugando ...')
+
+
   print("Fin del Juego")
     
 if(__name__ == "__main__"):
